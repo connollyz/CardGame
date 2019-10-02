7baed97408c54b97673449a65f84837a309a95e2
Zoe <zoe_connolly@hotmail.com>
2019-10-02 11:21:25 -0400

    Created components and now working on input section and storing in var

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    
+    <component :is="compToRender"></component>
   </div>
 </template>
 
 <script>
+import compForm from "./components/Form.vue"
+import compGame from "./components/Game.vue"
+import compResults from "./components/Results.vue"
   export default {
-
+    data(){
+      return{
+               compToRender: "compForm"
+           }
+    },
+    //resgerstering componets
+    components:{
+      compForm,
+      compGame,
+      compResults
+    }
   }
 </script>
 
---
+++ src/components/Form.vue
@@ -0,0 +1,68 @@
+<template>
+    <section class="playerForm">
+        <div class="instructions">
+            <h1>High &#38; Low Card Game!</h1>
+            <p><span>High card wins!</span></p>
+            <p>Pick your best hand 4, and play the CPU.</p>
+            <p>Valid cards include:</p>
+            <p> 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A</p>
+        </div>
+        <form>
+            <!--Get user name and save it in a variable.-->
+            <label for="userNameInput"> Enter your name</label>
+            <input 
+                type="text" 
+                name="userNameInput" 
+                id="userNameInpu" 
+                v-model.lazy="formData.userName" 
+                placeholder="Player name" 
+                minlength="3"
+                maxlength="10"
+                required
+            >
+
+            <!-- Get playersHand -->
+            <label for="playerHandInput"> Enter four cards</label>
+            <input 
+                type="text" 
+                name="playerHandInput" 
+                id="userNameInpu" 
+                v-model.lazy="formData.playerHand" 
+                placeholder="7 9 K 5" 
+                minlength="4"
+                maxlength="4"
+                required
+            >
+
+            
+
+            <!-- on Click prevent re-loading page and trigger functions -->
+            <button @click.prevent="submitForm">Play</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            formData:{
+                userName: '',
+                playerHand:[]
+            }
+        }
+    },
+    methods: {
+        submitForm(){
+            console.log('username',this.formData.userName)
+            console.log('PlayerHand',this.formData.playerHand)
+        }
+    },
+}
+</script>
+
+<style scoped>
+    label{
+        visibility:hidden;
+    }
+</style>
